54896e78e98374c6cb7b0e4bd5c1324c177ef76c
Cameron Mathos <haipayazoo@gmail.com>
2018-11-08 14:56:49 -0800

    Update project2.lisp

--- project2.lisp
+++ project2.lisp
@@ -52,21 +52,25 @@
 
 ; Gets a random number from -9 to 9
 (defun random_number ()
-  (let (num_list)
-    (setq num_list (list -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9))
+  (let ((num_list (list -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9)))
     (nth (random (length num_list)) num_list))
 )
 
 ; Gets a random variable x, y, or z
 (defun random_var ()
- (let (var_list)
-  (setq var_list (list 'x 'y 'z))
+ (let ((var_list (list 'x 'y 'z)))
   (nth (random (length var_list)) var_list))
 )
 
 ; Gets an either the '+', '-', or '*' operator
 (defun random_op ()
-  (let (op_list)
-    (setq op_list (list '+ '- '*))
+  (let ((op_list (list '+ '- '*)))
     (nth (random (length op_list)) op_list))
+)
+
+(defun test-expression(xval yval zval)
+  (setq x xval)
+  (setq y yval)
+  (setq z zval)
+  (eval (write (create_critter)))
 )
