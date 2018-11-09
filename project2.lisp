e1f5df587c9b4d9f47f8aa351f3ec1adbb35b58a
Cameron Mathos <haipayazoo@gmail.com>
2018-11-09 12:29:58 -0800

    Finished fitness evaluation

--- project2.lisp
+++ project2.lisp
@@ -11,6 +11,8 @@
 
 ; Creates a critter (random expression) to add to the population - uses
 ; recursion in order to create a deep expresssion based on a random number
+; Creates a critter (random expression)
+
 (defun create_critter ()
 
     ; The parent will always have an operator to be a legal expression
@@ -48,6 +50,7 @@
 
 )
 
+
 ; Creates a population of 50 critters (random expressions)
 (defun populate ()
 
@@ -66,9 +69,47 @@
   )
 
   ; Returns the population
-  population
+  pool
 
 )
+
+; Gets the fitness (which is the error) of each critter and forms
+; a list of fitness values of the pool
+(defun critter_fitness ()
+  (setq fitness_list '())
+  (setq test_samples (list
+         '(0 -2 1 -16)
+         '(-4 -5 -3 58)
+         '(9 8 -6 72)
+         '(9 -7 5 113)
+         '(-8 7 3 150)
+         '(5 4 -5 20)
+         '(6 -4 6 41)
+         '(-5 3 -7 -24)
+         '(-6 -5 9 -18)
+         '(1 0 2 2))
+  )
+
+  (dolist (critter pool)
+
+      (setq fitness 0)
+      
+      (dolist (sample test_samples)
+          (setq x (nth 0 sample))
+          (setq y (nth 1 sample))
+          (setq z (nth 2 sample))
+          (setq answer (nth 3 sample))
+
+          (setq output (eval critter))
+          (setq error (abs (- answer output)))
+          (setq fitness (+ fitness error))
+
+      )
+
+        (setq fitness_list (append fitness_list (list fitness)))
+    )
+)
+
 
 ; Gets a random number from -9 to 9
 (defun random_number ()
@@ -76,14 +117,24 @@
     (nth (random (length num_list)) num_list))
 )
 
+
 ; Gets a random variable x, y, or z
 (defun random_var ()
  (let ((var_list (list 'x 'y 'z)))
   (nth (random (length var_list)) var_list))
 )
 
+
 ; Gets an either the '+', '-', or '*' operator
 (defun random_op ()
   (let ((op_list (list '+ '- '*)))
     (nth (random (length op_list)) op_list))
 )
+
+(defun main ()
+    (populate)
+    (critter_fitness)
+    (format t "Fitness List: ~S~%" fitness_list)
+)
+
+(main)
